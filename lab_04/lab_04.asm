ab74eaf7b08f860d429981b0170f330631e0a05f
da-fuu <zharinov191@gmail.com>
2025-03-21 12:31:04 +0300

    removed all new regs, added saving during syscall

--- lab_04/lab_04.asm
+++ lab_04/lab_04.asm
@@ -10,29 +10,46 @@ format ELF64
 public _start    
 
 maxlen equ 9
+maxlen_str equ '9'
+newline equ 10
+
+macro syscall0 ; макрос сискола без аргументов
+{
+    push rcx
+    syscall
+    pop rcx
+}
 
 macro syscall1 a ; макрос сискола из 1 аргумента (номера функции)
 {
+    push rax
     mov rax,a
-    syscall
+    syscall0
+    pop rax
 }
 
 macro syscall2 a,b ; макрос сискола из 2 аргументов (первый - номер функции)
 {
+    push rdi
     mov rdi,b
     syscall1 a
+    pop rdi
 }
 
 macro syscall3 a,b,c ; макрос сискола из 3 аргументов (первый - номер функции)
 {
+    push rsi
     mov rsi,c
     syscall2 a,b
+    pop rsi
 }
 
 macro syscall4 a,b,c,d ; макрос сискола из 4 аргументов (первый - номер функции)
 {
+    push rdx
     mov rdx,d
     syscall3 a,b,c
+    pop rdx
 }
 
 macro print msg ; макрос печатает строку, аргумент - указатель на строку, обязана быть переменная с названием ptr_size
@@ -51,87 +68,109 @@ macro exit code ; макрос осуществляет выход из прог
 	syscall2 0x3C,code
 }
 
+macro getaddr row,col ; сохраняет адрес элемента в rbx
+{
+    imul rbx,row,maxlen
+    lea rbx,[matrix+rbx+col]
+}
+
+macro getelem row,col,out
+{
+    push rbx
+    getaddr row,col
+    mov out,[rbx]
+    pop rbx
+}
+
+macro setelem row,col,in
+{
+    push rbx
+    getaddr row,col
+    mov [rbx],in
+    pop rbx
+}
+
+macro for reg,start,stop
+{
+    mov reg,start
+    .#reg#_again:
+    cmp reg, stop
+    jge .#reg#_over
+}
+macro endfor reg
+{
+    inc reg
+    jmp .#reg#_again
+    .#reg#_over:
+}
+
 
 section '.text' executable
 
-read_num: ; функция читает число в r9b и пропускает один символ
+read_num: ; функция читает число в rdx и пропускает один символ
     syscall4 0x0,0,byte_buf,1
-    mov r9b,[byte_buf]
-    sub r9b,'0'
+    movzx rdx,[byte_buf]
+    sub rdx,'0'
     syscall4 0x0,0,byte_buf,1
     ret
 
 read_dims: ; функция читает колчество строк и столбцов в переменные rows и cols
     print in_rows
     call read_num
-    mov [rows],r9b
+    mov [rows],rdx
     
 	print in_cols
     call read_num
-    mov [cols],r9b
+    mov [cols],rdx
     ret
 
 read_matr: ; функция читает матрицу в переменную matrix, размер читает из переменных rows и cols
     print in_matr
     
-    mov al,[rows]
-    mul [cols]
-    mov r12b,al ; индекс элемента
-    mov rbx,matrix
-    .elem_loop:
-        call read_num
-        mov [rbx],r9b
-        inc rbx
-        dec r12b
-        cmp r12b,0
-        ja .elem_loop
+    for rsi,0,[rows]
+        for rdi,0,[cols]
+            call read_num
+            setelem rsi,rdi,dl
+        endfor rdi
+    endfor rsi
+  
     ret
     
 exchange_elems: ; функция изменяет матрицу в переменной matrix по заданию
-    mov rbx,matrix
-    mov r12b,[rows]
-    .row_loop:
-        mov r13b,[cols]
-        .col_loop:
-            inc rbx
-            cmp r13b,1
-            jbe .break_loop
-            
-            mov dl,[rbx-1]
+    
+    mov rax,[cols]
+    mov cl,2
+    div cl
+    mul cl
+    movzx rcx,al
+    
+    for rsi,0,[rows]
+        for rdi,0,rcx
+            getaddr rsi,rdi
+            mov dl,[rbx]
+            mov rax,rbx
+            inc rdi
+            getaddr rsi,rdi
             xchg dl,[rbx]
-            mov [rbx-1],dl
-            inc rbx
-            sub r13b,2
-            cmp r13b,0
-            ja .col_loop   
-        .break_loop:  
-        dec r12b
-        cmp r12b,0
-        ja .row_loop 
+            mov [rax],dl
+        endfor rdi
+    endfor rsi
+    
     ret
     
 print_matr: ; функция печатает матрицу в переменной matrix
     print out_matr
-        
-    mov rbx,matrix
-    mov r12b,[rows]
-    .row_loop:
-        mov r13b,[cols]
-        .col_loop:
-            mov dl,[rbx]
+     
+    for rsi,0,[rows]
+        for rdi,0,[cols]
+            getelem rsi,rdi,dl
             add dl,'0'
             write dl
-            write ' '
-            
-            inc rbx
-            dec r13b
-            cmp r13b,0
-            ja .col_loop    
+            write ' ' 
+        endfor rdi
+        write newline  
+    endfor rsi 
         
-        write 10  
-        dec r12b
-        cmp r12b,0
-        ja .row_loop
     ret
 
 _start: ; точка входа
@@ -148,23 +187,23 @@ _start: ; точка входа
 
 section '.rodata'
 
-in_rows db 'Введите количество строк матрицы от 1 до 9:', 10
+in_rows db 'Введите количество строк матрицы от 1 до ', maxlen_str, ':', newline
 in_rows_size = $-in_rows
 
-in_cols db 'Введите количество столбцов матрицы от 1 до 9:', 10
+in_cols db 'Введите количество столбцов матрицы от 1 до ', maxlen_str, ':', newline
 in_cols_size = $-in_cols
 
-in_matr db 'Введите элементы матрицы - цифры через 1 разделитель:', 10
+in_matr db 'Введите элементы матрицы - цифры через 1 разделитель:', newline
 in_matr_size = $-in_matr
 
-out_matr db 'Получившаяся матрица:', 10
+out_matr db 'Получившаяся матрица:', newline
 out_matr_size = $-out_matr
 
 
 section '.bss' writeable
 
-rows db ?
-cols db ?
+rows dq ?
+cols dq ?
 
 matrix_size = maxlen * maxlen
 
